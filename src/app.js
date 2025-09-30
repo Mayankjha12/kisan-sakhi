4cc0b5df1f7a93cf167abb35cd4a4d0e25c0de22
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2025-09-30 09:19:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState,useCallback } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FormSection from './components/FormSection';
